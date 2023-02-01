8ddb356fb8fded65418749af47d1e0eb43aaefcc
Pedro Silva <pedro@andoras.com.br>
2023-02-01 11:44:44 -0300

    Linear search algorithm

---
+++ linear.js
@@ -0,0 +1,25 @@
+//complexity: O(N)
+
+const linearSearch = (array, target) => {
+  let counter = 0;
+  for (let i = 0; i < array.length; i++) {
+    counter++;
+    if (array[i] === target) {
+      console.log(`${counter} steps to find the target.`);
+      return i;
+    }
+  }
+  return -1;
+};
+
+const array = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+  23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41,
+  42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
+  61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79,
+  80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98,
+  99, 100,
+];
+const target = 64;
+
+console.log(`The target's index is ${linearSearch(array, target)}`);
